0dbe75850d1edc3811632f911262a7b4c4eecd8f
Baron221 <baronjon080@gmail.com>
2023-05-10 21:46:52 +0900

    feat: callback va async functions

--- train.js
+++ train.js
@@ -1,3 +1,4 @@
+//Callback functionlar
 console.log("Jack Ma maslahatlari");
 const list = [
     "yaxshi talaba bo'ling", // 20
@@ -16,10 +17,10 @@ function maslahatBering(a,callback){
    else if(a<=40 && a<=50) callback(null,list[3]);
    else if(a<=50 && a<=60) callback(null,list[4]);
    else {
-       setTimeout(function(){
+       setInterval(function(){
            callback(null,list[5]);
 
-       },5000)
+       },1000)
    }
 }
 console.log("passed here 0")
@@ -31,3 +32,58 @@ maslahatBering(105,(err,data)=>{
 
 });
 console.log("passed here 1")
+
+
+// console.log("Jack Ma maslahatlari");
+// const list = [
+//     "yaxshi talaba bo'ling", // 20
+//     "to'gri boshliq tanlang va ko'proq xato qiling",//20-30
+//     "uzingizga ishlashni boshlang", //30-40
+//     "siz kuchli bo'lgan narsalarni qiling", //50-60
+//     "yoshlarga investitsya qiling", //50-60
+//     "endi dam oling "//60
+// ];
+
+// async function maslahatBering(a,callback){
+//    if (typeof a !== "number") throw new Error("please input number ",null);
+//    else if(a<=20 ) return list[0];
+//    else if(a<=20 && a<=30)  return list[1];
+//    else if(a<=30 && a<=40)  return list[2];
+//    else if(a<=40 && a<=50)  return list[3];
+//    else if(a<=50 && a<=60)  return list[4];
+//    else {
+//        return new Promise((resolve,reject)=>{
+//             //   setTimeout(function(){
+//             //      resolve(list[5]);
+       
+//             //   },5000)
+            
+
+            
+//                 setInterval(function(){
+//                   resolve(list[5]);
+       
+//                   },1000)
+            
+//        })
+ 
+//    }
+// }
+// console.log("passed here 0")
+// maslahatBering(105)
+// .then((data)=>{
+//     console.log('javob',data)
+// }).catch((err)=>{
+//     console.log("ERROR:", err)
+// });
+// console.log("passed here 1")
+
+//async /await
+
+// async function run(){
+//     let answer =await maslahatBering(65);
+//     console.log(answer);
+
+// }
+
+// run()
