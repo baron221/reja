3a4e1a2f3b15f2bd45406bac7df7417443714152
Baron221 <baronjon080@gmail.com>
2023-05-15 15:22:44 +0900

    feat: Frontend javascriptni qurish

--- app.js
+++ app.js
@@ -33,12 +33,8 @@ app.post("/create-item",(req,res)=>{
     console.log("user entered /");
     const new_reja = req.body.reja;
     db.collection("plans").insertOne({reja:new_reja},(err,data)=>{
-        if(err){
-            console.log(err);
-            res.end("something went wrong");
-        }else{
-            res.end("successfully added");
-        }
+     console.log(data.ops);
+     res.json(data.ops[0]);
     });
 });
 
