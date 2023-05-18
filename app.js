d9ff7bbed6aceb9d816fc5f1aaf2bebf3d75cb62
Baron221 <baronjon080@gmail.com>
2023-05-18 16:52:18 +0900

    feat:Raja loyihaning yakuniy bosqichi

--- app.js
+++ app.js
@@ -46,10 +46,26 @@ db.collection("plans").deleteOne({_id:new mongodb.ObjectId(id)}, function(err,da
 })
 
 });
+app.post("/edit-item",(req,res)=>{
+   const data = req.body;
+   console.log(data);
+   db.collection("plans").findOneAndUpdate({_id: new mongodb.ObjectId(data.id)},
+   {$set:{reja:data.new_input}}, 
+   function(err,data){
+    res.json({state : "success"});
+   })
 
-// app.get("/author",function(req,res){
-//     res.render("author",{user:user})
-// });
+
+});
+
+app.post("/delete-all",(req, res) =>{
+    if(req.body.delete_all){
+        db.collection("plans").deleteMany(function(){
+            res.json({state:"all plans deleted"});
+        });
+    }
+});
+
 
 app.get("/",function(req,res){
     console.log("user entered /")
