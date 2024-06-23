88edc4a13513195cf568d69da9719d5fbe178521
Baron221 <baronjon080@gmail.com>
2024-06-23 22:36:56 +0900

    feat:create docker file

--- train.js
+++ train.js
@@ -1,23 +1,26 @@
-function tubSon(num)
-{
-  if (num===1)
-  {
-    return false;
-  }
-  else if(num === 2)
-  {
-    return true;
-  }else
-  {
-    for(var x = 2; x < num; x++)
-    {
-      if(num % x === 0)
-      {
-        return false;
-      }
-    }
-    return true;  
-  }
-}
+const detail_list = [12,23,null,34,null,32,null,null];
+const result1 = moveNullsKeepOrder(detail_list);
+console.log(`result 1: ${result1}`);
 
-console.log(tubSon(5));
+function moveNullsKeepOrder(detail_list){
+  return detail_list.sort((a,b)=>{
+    if(a === null){
+      return 1;
+    };
+
+    if(b === null){
+      return -1;
+    };
+
+    if(a === b){
+      return 0
+    }
+    return a < b ? -1 : 1;
+
+  }
+
+)};
+
+// const detail_list = [12,23,null,34,null,32,null,null];
+// const result1 = moveNullsKeepOrder(detail_list);
+// console.log(`result 1 ${result1}`);
